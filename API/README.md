1248a19cbaa71806e1bd573aceeaa528b0338325
DonJoel <jt3bk@virginia.edu>
2019-08-07 13:01:04 +0200

    working profile

---
+++ API/README.md
@@ -0,0 +1,1 @@
+# fsbnb-api
--- API/src/database/database.js
+++ API/src/database/database.js
@@ -10,7 +10,16 @@ const config = {
   database: "fs_bnb"
 };
 
-var connection = mysql.createConnection(config);
+// const config = {
+//   host: "us-cdbr-iron-east-02.cleardb.net",
+//   port: 3306,
+//   user: "bc05126ff0e150",
+//   password: "6910055d",
+//   database: "heroku_a3ee1eece26bd24"
+// };
+
+//bc05126ff0e150:6910055d@us-cdbr-iron-east-02.cleardb.net/heroku_a3ee1eece26bd24?reconnect=true
+mysql: var connection = mysql.createConnection(config);
 
 connection.connect(err => {
   if (err) console.log(err);
